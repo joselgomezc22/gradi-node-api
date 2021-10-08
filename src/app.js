0909298cd38358a18ea0b71f973e00b97cbc037c
Jose Leomar Gomez <61978606+joselgomezc22@users.noreply.github.com>
2021-10-08 16:51:27 -0500

    ngrok app

--- src/app.js
+++ src/app.js
@@ -14,24 +14,16 @@ app.listen(3000, () => {
   console.log("Your app is now listening on port 3000");
 });
 
-const credentials = {
-  apikey: process.env.API_KEY,
-  password: process.env.PASSWORD,
-  hostname: process.env.SHOP,
-  version: process.env.VERSION,
-};
+const credentials = process.env;
 
-// Get count of fullfilment orders
 app.get("/orderscount", async (req, res) => {
-    
   try {
-    const resource = "orders";
-    const url = `https://e904a293327099bd8cb00513b1af40a4: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@joseleomar-gradi-store.myshopify.com/admin/api/2021-10/orders.json`;
+    const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/orders/count.json?status=open`;
     //const url = `https://jsonplaceholder.typicode.com/todos/1`;
     const response = await axios.get(url);
     const ret = response.data
-    return res.json({
-      data: ret,
+    return res.json({ 
+      data: ret
     });
   } catch (error) {
     console.error("Generó error", error);
@@ -39,4 +31,23 @@ app.get("/orderscount", async (req, res) => {
       message: "Falló al realizar la petición",
     });
   }
-}) 
+}); 
+
+app.get("/customerscount", async (req, res) => {
+  try {
+    const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/customers/count.json`;
+    //const url = `https://jsonplaceholder.typicode.com/todos/1`;
+    const response = await axios.get(url);
+    const ret = response.data
+    return res.json({ 
+      data: ret,
+    });
+  } catch (error) {
+    console.error("Generó error", error);
+    const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/customers/count.json`;
+    return res.status(500).json({
+      message: "Falló al realizar la petición",
+      url:url
+    });
+  }
+}); 
