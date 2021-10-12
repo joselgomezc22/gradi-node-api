cdd29042be3817cf08e4c23764b6fe7127a64563
Jose Leomar Gomez <61978606+joselgomezc22@users.noreply.github.com>
2021-10-11 22:24:23 -0500

    Shopify Hook Suscribed & Nndemailer Installed

--- src/app.js
+++ src/app.js
@@ -36,7 +36,6 @@ app.get("/orderscount", async (req, res) => {
 app.get("/customerscount", async (req, res) => {
   try {
     const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/customers/count.json`;
-    //const url = `https://jsonplaceholder.typicode.com/todos/1`;
     const response = await axios.get(url);
     const ret = response.data
     return res.json({ 
@@ -45,6 +44,31 @@ app.get("/customerscount", async (req, res) => {
   } catch (error) {
     console.error("Generó error", error);
     const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/customers/count.json`;
+    return res.status(500).json({
+      message: "Falló al realizar la petición",
+      url:url
+    });
+  }
+}); 
+app.get("/createhook", async (req, res) => {
+  try {
+    let obj = {
+      "webhook": {
+        "topic": "orders/create",
+        "address": "https://c7a1-186-154-120-65.ngrok.io/createdOrderHook",
+        "format": "json"
+        }
+      };
+
+    const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/webhooks.json`;
+    const response = await axios.post(url,obj);
+    const ret = response.data
+    return res.json({ 
+      data: ret,
+    });
+  } catch (error) {
+    console.error("Generó error", error);
+    const url = `https://${credentials.API_KEY}:${credentials.API_SECRET_KEY}@${credentials.SHOP}/admin/api/2021-10/webhooks.json`;
     return res.status(500).json({
       message: "Falló al realizar la petición",
       url:url
